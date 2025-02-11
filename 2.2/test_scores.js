547ad87776323f8f36a2ea7cd31130b7b844132e
alexestrada1 <estradaalex46@gmail.com>
2025-02-11 08:37:32 -0600

    futue vales has been finished

--- 2.2/test_scores.js
+++ 2.2/test_scores.js
@@ -1,20 +1,34 @@
-"use strict";
+// declare and initialize an array to hold test scores from user
+const scores = [];
+ 
+// use do-while loop to get scores from the user and store in array
+let score = 0;
+do {
+    score = parseInt(prompt(
+        "Enter a test score, or enter -1 to end scores", -1));
+ 
+    if (score >= 0 && score <= 100) {
+        scores[scores.length] = score;
+    }
+    else if (score != -1){
+        alert("Score must by a valid number from 0 through 100");
+    }
+}
+while(score != -1);
 
-let total = 0;
-
-const score1 = parseInt(prompt("Enter test score"));
-total += score1;
-
-const score2 = parseInt(prompt("Enter test score"));
-total += score2;
-
-const score3 = parseInt(prompt("Enter test score"));
-total += score3;
-
-const average = parseInt(total/3);
-
-const html = "<p>Score 1 = " + score1 + "</p>" +
-    "<p>Score 2 = " + score2 + "</p>" +
-    "<p>Score 3 = " + score3 + "</p>" +
-    "<p>Average score = " + average + "</p>";
-document.write(html);
+if (scores.length > 0) {
+    // use a for-in loop to add each score to total and display it
+    let total = 0;
+    let index = 0;
+    for (let i of scores) {
+        total += i;
+        index++;
+        document.write(
+            `<p>Score ${parseInt(index)}: ${i}</p>`);
+    }
+ 
+    //calculate and display the average
+    const average = parseInt(total/scores.length);
+    document.write(`<p>Average score is ${average}</p>`);
+}
+ 
---
+++ 3.1/futureValue.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// get investment amount - loop until user enters a number
+let extraEntry = "";
+
+do{
+let investment = 0;
+do {
+    investment = parseFloat(
+        prompt("Enter investment amount as xxxxx.xx", 10000));
+}
+while ( isNaN(investment) || investment<=0 );
+
+// get interest rate - loop until user enters a number
+let rate = 0;
+do {
+    rate = parseFloat(prompt("Enter interest rate as xx.x", 7.5));
+}
+while ( isNaN(rate) || rate <= 0 || rate >= 15);
+
+// get number of years - loop until user enters a number
+let years = 0;
+do {
+    years = parseInt(prompt("Enter number of years", 10));
+}
+while ( isNaN(years) || years <= 0 );
+
+// write entries
+document.write(`<h4>Investment amount = ${investment} Interest rate = ${rate} Years = ${years}</h4>`);
+
+// calulate future value
+let futureValue = investment;
+for (let i = 1; i <= years; i++ ) {
+        const interest = futureValue * rate / 100;
+        futureValue = futureValue + interest;
+
+    // write results
+    document.write(`<p>Year=${i} Interest=${interest.toFixed(2)} Value=${futureValue.toFixed(2)}</p>`);
+}
+extraEntry = prompt("Another Entry: y or n", "n");
+
+}
+while(extraEntry != "n");
